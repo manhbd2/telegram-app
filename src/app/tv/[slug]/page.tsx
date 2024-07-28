4880c1ba13a609b573edd8604d31bf1dde4ddc38
manhbd2 <buiducmanhnb35@gmail.com>
2024-07-28 22:00:01 +0700

    re build

---
+++ src/app/tv/[slug]/page.tsx
@@ -0,0 +1,27 @@
+import MovieDetail from '@/components/MovieDetail';
+import { siteConfig } from '@/configs/site';
+import { getIdFromSlug } from '@/libs/utils';
+import MovieService from '@/services/MovieService';
+import type { ShowWithGenreAndVideo } from '@/types/movie';
+import { MediaType } from '@/types/movie';
+
+type Props = {
+  params: { slug: string };
+};
+
+export default async function TvShowPage({ params }: Props) {
+  const id: number = getIdFromSlug(params.slug);
+  const movie: ShowWithGenreAndVideo = await MovieService.findMovieByIdAndType(
+    id,
+    MediaType.TV,
+  );
+
+  const h1 = `${siteConfig.name} - Tv Detail`;
+
+  return (
+    <main className="min-h-screen">
+      <h1 className="hidden">{h1}</h1>
+      <MovieDetail show={movie} />
+    </main>
+  );
+}
