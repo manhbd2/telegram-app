dd22f5cc73a77fa08d126beff274152ca6b30342
Manh Bui <manhbd2@msb.com.vn>
2024-07-29 17:42:42 +0700

    update

---
+++ src/components/MovieWatch.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+type IMovieWatchProps = {
+    show: ShowWithGenreAndVideo;
+  };
+function MovieWatch(props) {
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default MovieWatch;
