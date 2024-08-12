c3d922219a38741342cb18f3b9ffca02594adb95
manhbd2 <buiducmanhnb35@gmail.com>
2024-08-12 19:18:10 +0700

    update

--- src/components/shows/Header.tsx
+++ src/components/shows/Header.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import React from 'react';
 
-import { getImageUrl, getWatchPath } from '@/libs/movie';
+import { getDetailPath, getImageUrl } from '@/libs/movie';
 import type { Show } from '@/types/movie';
 
 import { Icons } from '../icons/icons';
@@ -30,10 +30,7 @@ function Header(props: IHeaderProps) {
             <div className="absolute inset-0 z-10">
               <div className="absolute bottom-20 w-full">
                 <div className="mt-4 flex items-center justify-center gap-x-2">
-                  <Link
-                    prefetch={false}
-                    href={getWatchPath(show?.id, show?.media_type)}
-                  >
+                  <Link prefetch={false} href={getDetailPath(show)}>
                     <Button
                       aria-label="Play video"
                       className="h-auto shrink-0 gap-2 rounded-sm px-9 py-[6px]"
@@ -45,21 +42,23 @@ function Header(props: IHeaderProps) {
                       Play
                     </Button>
                   </Link>
-                  <Button
-                    aria-label="Open show's details modal"
-                    variant="outline"
-                    className="h-auto shrink-0 gap-2 rounded-sm px-9 py-[6px]"
-                  >
-                    <Icons.Info aria-hidden="true" className="size-5" />
-                    Info
-                  </Button>
+                  <Link prefetch={false} href={getDetailPath(show)}>
+                    <Button
+                      aria-label="Open show's details modal"
+                      variant="outline"
+                      className="h-auto shrink-0 gap-2 rounded-sm px-9 py-[6px]"
+                    >
+                      <Icons.Info aria-hidden="true" className="size-5" />
+                      Info
+                    </Button>
+                  </Link>
                 </div>
               </div>
             </div>{' '}
             <div className="absolute inset-0 right-[12.09%] z-[8] bg-gradient-to-r from-secondary to-85%" />
             <div className="absolute inset-x-0 -bottom-px z-[8] h-[14.7vw] bg-gradient-to-b from-background/0 from-30% via-background/30 via-50% to-background to-80%" />
           </div>
-          <div className="relative inset-0 -z-50 mb-5 pb-[70%] sm:pb-[40%]" />
+          <div className="relative inset-0 -z-50 mb-5 pb-[80%] sm:pb-[40%]" />
         </>
       )}
     </section>
